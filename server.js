504fcc414512f177bb2cf60235b7b9313916a31c
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-07-10 18:18:45 +0530

    express app get api GET All

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 
-const app = new express()
-const port=process.env.PORT || 5001
+const app = express()
+const port=process.env.PORT || 5000
 
+app.get("/api/contacts", (req,res)=>{//do not forget to put / infront of api/contacts else error
+    //res.send("Get all contacts")
+    res.status(200).json({message:"Get All Contacts API Response"})
+})
 
 app.listen(port, ()=>{
     console.log(`Server running at ${port} PORT`)
