3d1fb668910964368ea8d8019cb5d9f5c92f7e40
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-07-10 18:35:12 +0530

    multiple routes created using express router() and route method

--- routes/contactsRoutes.js
+++ routes/contactsRoutes.js
@@ -6,5 +6,21 @@ router.route("/").get((req,res)=>{
     res.status(200).json({message:"Get All Contacts API Response"})
 })
 
+router.route("/:id").get((req,res)=>{
+    res.status(200).json({message:"Get Single Contact API Response"})
+})
+
+router.route("/:id").put((req,res)=>{
+    res.status(200).json({message:"Update A Contact API Response"})
+})
+
+router.route("/").post((req,res)=>{
+    res.status(200).json({message:"Create A Contact API Response"})
+})
+
+router.route("/:id").delete((req,res)=>{
+    res.status(200).json({message:"Delete A Contact API Response"})
+})
+
 
 module.exports = router
