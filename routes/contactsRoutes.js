df2e75f506a84a9e0ad36c236fe73350807be668
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-07-10 18:32:31 +0530

    server.js use middleware app.use(relative route url, route)|| router get route created

---
+++ routes/contactsRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.route("/").get((req,res)=>{
+    res.status(200).json({message:"Get All Contacts API Response"})
+})
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-
+const contactsRouter = require('./routes/contactsRoutes')
 const app = express()
 const port=process.env.PORT || 5000
 
-app.get("/api/contacts", (req,res)=>{//do not forget to put / infront of api/contacts else error
-    //res.send("Get all contacts")
-    res.status(200).json({message:"Get All Contacts API Response"})
-})
+app.use("/api/contacts", contactsRouter);
 
 app.listen(port, ()=>{
     console.log(`Server running at ${port} PORT`)
