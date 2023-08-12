38e69413edeb610122ef8cf9229f65106e9f7a21
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-08-12 23:44:44 +0530

    trying to fix cors

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", usersRouter);
