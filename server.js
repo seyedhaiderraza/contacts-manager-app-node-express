e0884eee25b94e6c82cfd142bfcc9ab4bcd7250b
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-08-12 23:23:00 +0530

    added cors policy for localhost:3000 react client access

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const errorHandler = require('./middleware/errorHandler')
-const connectDB = require('./config/dbConnection')
-const contactsRouter = require('./routes/contactsRoutes')
-const usersRouter = require('./routes/usersRoutes')
-connectDB()
-const app = express()
-const port=process.env.PORT || 5000
+const express = require("express");
+const dotenv = require("dotenv").config();
+const errorHandler = require("./middleware/errorHandler");
+const connectDB = require("./config/dbConnection");
+const contactsRouter = require("./routes/contactsRoutes");
+const usersRouter = require("./routes/usersRoutes");
+const cors = require("cors");
+connectDB();
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use("/api/contacts", contactsRouter);
-app.use("/api/users", usersRouter)
-app.use(errorHandler)
+app.use("/api/users", usersRouter);
+app.use(errorHandler);
 
-
-app.listen(port, ()=>{
-    console.log(`Server running at ${port} PORT`)
-})
+app.listen(port, () => {
+  console.log(`Server running at ${port} PORT`);
+});
